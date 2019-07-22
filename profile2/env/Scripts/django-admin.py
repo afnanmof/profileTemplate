91412484435787f9427b586778c3a614d32a62cd
Afnan Mofti <furry.fur@hotmail.com>
2019-07-22 23:11:03 +0300

    adding

---
+++ profile2/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\OMHASSAN\Documents\Project\profile2\profile2\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
